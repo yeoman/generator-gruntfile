b33a30253fc320e8b8022df513ea97f400517731
Addy Osmani <addyosmani@gmail.com>
2013-10-01 14:15:59 +0100

    Remove cruft, make this actually work

--- app/index.js
+++ app/index.js
@@ -7,9 +7,11 @@ var grunt = require('grunt');
 var GruntfileGenerator = module.exports = function GruntfileGenerator(args, options) {
   yeoman.generators.Base.apply(this, arguments);
 
+/*
   this.on('end', function () {
     this.installDependencies({ skipInstall: options['skip-install'] });
   });
+*/
 
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
@@ -22,20 +24,29 @@ GruntfileGenerator.prototype.askFor = function askFor() {
 
   // welcome message
   console.log(this.yeoman);
+  console.log('This template tries to guess file and directory paths, but ' +
+  'you will most likely need to edit the generated Gruntfile.js file before ' +
+  'running grunt. _If you run grunt after generating the Gruntfile, and ' +
+  'it exits with errors, edit the file!_');
 
   var prompts = [{
-    type: 'confirm',
-    name: 'dom',
-    message: 'Is the DOM involved in ANY way?'
-  }, {
-    type: 'confirm',
-    name: 'minConcat',
-    message: 'Will files be concatenated or minified?'
-  }, {
-    type: 'confirm',
-    name: 'packageJSON',
-    message: 'Will you have a package.json file?'
-  }];
+    type: 'checkbox',
+    name: 'features',
+    message: 'What more would you like?',
+    choices: [{
+      name: 'Is the DOM involved in ANY way?',
+      value: 'dom',
+      checked: false
+    }, {
+      name: 'Will files be concatenated or minified?',
+      value: 'minConcat',
+      checked: false
+    }, {
+      name: 'Will you have a package.json file?',
+      value: 'packageJSON',
+      checked: false
+    }]
+  }]
 
   // Find the first `preferred` item existing in `arr`.
   function prefer(arr, preferred) {
@@ -55,11 +66,14 @@ GruntfileGenerator.prototype.askFor = function askFor() {
   this.jquery = grunt.file.expand({ filter: 'isFile' }, '**/jquery*.js').length > 0;
 
   this.prompt(prompts, function (props) {
-    this.dom = props.dom;
-    this.minConcat = props.minConcat;
-    this.packageJSON = props.packageJSON;
 
-    this.testTask = props.dom ? 'qunit' : 'nodeunit';
+    var features = props.features;
+
+    this.dom = features.dom;
+    this.minConcat = features.minConcat;
+    this.packageJSON = features.packageJSON;
+
+    this.testTask = features.dom ? 'qunit' : 'nodeunit';
     this.fileName = props.packageJSON ? '<%= pkg.name %>' : 'FILE_NAME';
 
     cb();
